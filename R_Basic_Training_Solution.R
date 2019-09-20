ccfed288f36df5f52a2b92cb5b3cf42d6b1baecd
Sobil Dalal <sobil.dalal@agrevolution.in>
2019-09-20 10:43:16 +0530

    On branch master
    Initial commit
    
    Changes to be committed:
    	new file:   .RData
    	new file:   .Rhistory
    	new file:   R_Basic_Training_Solution.R

---
+++ R_Basic_Training_Solution.R
@@ -0,0 +1,24 @@
+print("1) What is two to the power of five?")
+print(2^5)
+
+print("2) Create a vector called stock.prices with the following data points: 23,27,23,21,34")
+stock.prices <- c(23,27,23,21,34)
+print(stock.prices)
+
+print("3) Assign names to the price data points relating to the day of the week, starting with Mon, Tue, Wed, etc...")
+names(stock.prices) <- c('Mon','Tues','Wed','Thu','Fri')
+print(stock.prices)
+
+print("4) What was the average (mean) stock price for the week? (You may need to reference a built-in function)")
+print(mean(stock.prices))
+
+print("5 Create a vector called over.23 consisting of logicals that correspond to the days where the stock price was more than $23")
+over.23 <- stock.prices > 23
+print(over.23)
+
+print("6) Use the over.23 vector to filter out the stock.prices vector and only return the day and prices where the price was over $23")
+print(stock.prices[over.23])
+
+print("7) Use a built-in function to find the day the price was the highest")
+max.price <- stock.prices == max(stock.prices)
+print(stock.prices[max.price])
